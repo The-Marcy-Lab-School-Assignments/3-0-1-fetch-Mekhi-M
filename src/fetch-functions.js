9ec832a63dd28d0e566c34c90820195565727927
Mekhi-M <mekhim0731@gmail.com>
2024-06-12 07:31:27 -0400

    q5 + start fetch-funcs

--- src/fetch-functions.js
+++ src/fetch-functions.js
@@ -1,13 +1,13 @@
-const userUrl = 'https://jsonplaceholder.typicode.com/users'
+const userUrl = "https://jsonplaceholder.typicode.com/users";
 
 export const checkResponseStatus = () => {
+  return fetch(`https://jsonplaceholder.typicode.com/users`);
 };
 
 export const getUsers = () => {
+  return fetch(`https://jsonplaceholder.typicode.com/users`).then((response) => );
 };
 
-export const getUserPosts = () => {
-};
+export const getUserPosts = () => {};
 
-export const createNewUser = () => {
-}
+export const createNewUser = () => {};
